a50cc28f42f4a86f7a94103c53f38d5d6d324046
Oelin <42823429+Oelin@users.noreply.github.com>
2021-01-14 17:10:02 +0000

    Update lex.js

--- src/lex.js
+++ src/lex.js
@@ -7,87 +7,86 @@ function use(s) {
 
 
 function here() {
-	return code
+  return code
 }
 
 
 function move(m) {
-	let v = m[m.length - 1]
-	code = text.slice(v.length)
-
-	return v
+  let v = m[m.length - 1]
+  code = text.slice(v.length)
+  
+  return v
 }
 
 
 function itch(token) {
-	let m 
-
-	if (m = code.match(token)) {
-		return move(m)
-	}
-
-	else fail()
+  let m 
+  
+  if (m = code.match(token))
+    return move(m)
+  
+  fail()
 }
 
 
 // token types
 
 function string() {
-	return itch(/^'[^']*'/)
+  return itch(/^'[^']*'/)
 }
 
 
 function number() {
-	return itch(/^(0x[0-9a-fA-F]+|0b[01]+|\d+\.\d+|\d+)/)
+  return itch(/^(0x[0-9a-fA-F]+|0b[01]+|\d+\.\d+|\d+)/)
 }
 
 
 function punc() {
-	return itch(/^[\(\)\[\]\,]|^\s*(\->)/)
+  return itch(/^[\(\)\[\]\,]|^\s*(\->)/)
 }
 
 
 function operator() {
-	return itch(/^((and|or|not)(\W|$)|==|!=|<<|>>|<<=|>>=|<=|>=|<|>|\+=|\-=|\*\*=|\*=|\/=|&=|\|=|\^=|=|\+|\-|\*\*|\*|\/|&|\||\^|~|\.|\[|\()/)
+  return itch(/^((and|or|not)(\W|$)|==|!=|<<|>>|<<=|>>=|<=|>=|<|>|\+=|\-=|\*\*=|\*=|\/=|&=|\|=|\^=|=|\+|\-|\*\*|\*|\/|&|\||\^|~|\.|\[|\()/)
 }
 
 
 function keyword() {
-	return itch(/^(if|elif|else|while|for|in|def|return|break|continue|end)(?:\W|$)/)
+  return itch(/^(if|elif|else|while|for|in|def|return|break|continue|end)(?:\W|$)/)
 }
 
 
 function ident() {
-	return itch(/^(?:(?!(if|elif|else|while|for|in|def|return|break|continue|end|and|or|not)(\W|$)))[a-zA-Z_][\w_]*/)
+  return itch(/^(?:(?!(if|elif|else|while|for|in|def|return|break|continue|end|and|or|not)(\W|$)))[a-zA-Z_][\w_]*/)
 }
 
 
 function space() {
-	return itch(/^\s+/)
+  return itch(/^\s+/)
 }
 
 
 function end() {
-	return itch(/^$/)
+  return itch(/^$/)
 }
 
 
 function fail() {
-	throw 'syntax error'
+  throw 'syntax error'
 }
 
 
 module.exports = {
-	use,
-	peek,
-	skip,
-	string,
-	number,
-	punc,
-	operator,
-	keyword,
-	ident,
-	space,
-	end,
-	fail
+  use,
+  peek,
+  skip,
+  string,
+  number,
+  punc,
+  operator,
+  keyword,
+  ident,
+  space,
+  end,
+  fail
 }
