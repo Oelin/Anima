e9bac4cc24759401e7bef1a1ffbb9eccfc88b433
Oelin <42823429+Oelin@users.noreply.github.com>
2021-01-24 16:43:23 +0000

    Update parse.js

--- src/parser/parse.js
+++ src/parser/parse.js
@@ -1,3 +1,4 @@
 const { use, itch, keep, peek, fail, string, number, punc, operator, keyword, ident, space, end } = require('./lex')
-const { prefix, infix, precedence } = require('./precedence')
-const 
+const { unary, binary, tight } = require('./operator')
+
+// todo...
