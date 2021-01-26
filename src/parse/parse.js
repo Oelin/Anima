5ae766c4ee8e3492dd993bf5bea79a8e85bbac5d
Oelin <oel1n@protonmail.com>
2021-01-26 17:10:27 +0000

    update

--- src/parse/parse.js
+++ src/parse/parse.js
@@ -1,4 +1,28 @@
-const { use, itch, keep, peek, fail, string, number, punc, operator, keyword, ident, space, end } = require('./lex')
-const { unary, binary, flip } = require('./expr.json')
+imports('./lex')
+imports('./operator')
 
+
+function name() {
+  return pad(ident)
+}
+
+
+function list() {
+  return some(expr, /^[/, ',', /^]/)
+}
+
+
+// helpers
+
+function imports(path) {
+  Object.assign(global, require(path))
+}
+
+function pad(parser) {
+  skip(space)
+  let node = parser()
+  skip(space)
+
+  return node
+}
 // todo...
