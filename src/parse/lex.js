b5ca7c78309a12f64a80af401bc9ee604b8b81d5
Oelin <42823429+Oelin@users.noreply.github.com>
2021-01-29 09:24:38 +0000

    Update lex.js

--- src/parse/lex.js
+++ src/parse/lex.js
@@ -1,81 +1,20 @@
-let text
+let code
 
 
 function use(s) {
-  text = s
+  code = s
 }
 
 
 function here() {
-  return text
+  return code
 }
 
-
-// token types
-
-let osq = () => itch(/^\[/)
-let csq = () => itch(/^\]/)
-let obr = () => itch(/^\(/)
-let cbr = () => itch(/^\)/)
-let comma = () => itch(/^,/)
-
-
-function string() {
-  return itch(/^'[^']*'/)
-}
-
-
-function number() {
-  return itch(/^(0x[0-9a-fA-F]+|0b[01]+|\d+\.\d+|\d+)/)
-}
-
-
-function punc() {
-  return itch(/^[\(\)\[\]\,]|^\s*(\->)/)
-}
-
-
-function arrow() {
-  return itch(/^->/)
-}
-
-
-function operator() {
-  return itch(/^((and|or|not)(\W|$)|==|!=|<<|>>|<<=|>>=|<=|>=|<|>|\+=|\-=|\*\*=|\*=|\/=|&=|\|=|\^=|=|\+|\-|\*\*|\*|\/|&|\||\^|~|\.|\[|\()/)
-}
-
-
-function keyword() {
-  return itch(/^(if|elif|else|while|for|in|def|return|break|continue|end)(?:\W|$)/)
-}
-
-
-function ident() {
-  return itch(/^(?:(?!(if|elif|else|while|for|in|def|return|break|continue|end|and|or|not)(\W|$)))([a-zA-Z_][\w_]*)/)
-}
-
-
-function space() {
-  return itch(/^\s+/)
-}
-
-
-function end() {
-  return itch(/^$/)
-}
-
-
-function fail() {
-  throw	'syntax error'
-}
-
-
-// scanner
 
 function itch(token) {
   let m 
   
-  if (m = text.match(token))
+  if (m = code.match(token))
     return move(m)
   
   fail()
@@ -84,7 +23,7 @@ function itch(token) {
 
 function move(m) {
   let v = m[m.length - 1]
-  use(text.slice(v.length))
+  use(code.slice(v.length))
   
   return v
 }
@@ -117,19 +56,4 @@ module.exports = {
   itch,
   skip,
   peek,
-  fail,
-  string,
-  number,
-  punc,
-  operator,
-  keyword,
-  ident,
-  space,
-  end,
-  osq,
-  csq,
-  obr,
-  cbr,
-  comma,
-  arrow
 }
