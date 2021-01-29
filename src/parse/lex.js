1a1b841d7b1d8b61fddb5b6a9f9825b45abab1f8
Oelin <42823429+Oelin@users.noreply.github.com>
2021-01-29 11:20:46 +0000

    Update lex.js

--- src/parse/lex.js
+++ src/parse/lex.js
@@ -8,7 +8,7 @@ function use(s) {
 function need(token) {
   if (m = code.match(token))
     return move(m)
-  
+
   throw ''
 }
 
@@ -17,7 +17,7 @@ function need(token) {
 
 function peek(...a) {
   let s = code
-  let node = keep(...a)
+  let node = skip(...a)
   use(s)
 
   return node
@@ -26,10 +26,10 @@ function peek(...a) {
 
 function skip(p, ...a) {
   let s = code
-  
-  try { 
-    return p(...a) 
-  } catch { 
+
+  try {
+    return p(...a)
+  } catch {
     use(s)
   }
 }
@@ -38,16 +38,16 @@ function skip(p, ...a) {
 // advance
 
 function move(m) {
-  let v = m[m.length - 1]
+  let v = m.filter(e => e).pop()
   code = code.slice(v.length)
-  
+
   return v
 }
 
 
-module.exports = { 
-  use, 
-  need, 
-  peek, 
-  skip 
+module.exports = {
+  use,
+  need,
+  peek,
+  skip
 }
